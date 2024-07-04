bb5d87358d466f81033fc9a28365390b2fa6df3f
marknaman05 <marknaman05@gmail.com>
2024-07-04 08:30:35 +0530

    Indus Dynatech Static Page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div className="App">
+      <header className="App-header">
+        <img src="public\logo.png" className="App-logo" alt="logo" />
+        <h1>Welcome to Indus Dynatech Solutions</h1>
+        <p>The Website is under Development ⚠️</p>
+      </header>
+      <footer className="App-footer">
+        <h3>Ajay Markhedkar +91 7982993011</h3>
+      </footer>
+    </div>
+    
+  )
+}
+
+export default App
