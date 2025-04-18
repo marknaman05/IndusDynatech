150060c48d17bbf5d577b11e01e80138154c7136
marknaman05 <marknaman05@gmail.com>
2025-04-18 20:02:38 +0530

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -35,6 +36,7 @@ function AppContent() {
         </AnimatePresence>
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
